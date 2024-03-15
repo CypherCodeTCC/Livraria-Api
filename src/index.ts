3b2ccc07f9a84755d93345174eddf81d877ecff7
3DUTEX <t3xdev@gmail.com>
2024-03-15 12:35:33 -0300

    alterar porta para 3000

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@
 // Imports modules
 import app from "./server";
 
-const PORT: number = 3002;
+const PORT: number = 3000;
 app.listen(PORT, () => {
   console.log(`server is running on http://localhost:${PORT}`);
 });
