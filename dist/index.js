54015270399d8021c54589aa8ee7f3f8a271bf9c
3DUTEX <t3xdev@gmail.com>
2024-03-15 12:43:38 -0300

    alterar ccaminho do script start

--- dist/index.js
+++ dist/index.js
@@ -1,8 +1,10 @@
 "use strict";
 // init server
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
+var __importDefault =
+    (this && this.__importDefault) ||
+    function (mod) {
+        return mod && mod.__esModule ? mod : { default: mod };
+    };
 Object.defineProperty(exports, "__esModule", { value: true });
 // Imports modules
 const server_1 = __importDefault(require("./server"));
