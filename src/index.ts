fd7ef116789fa976eb15f8c051717cff49451f84
3DUTEX <t3xdev@gmail.com>
2024-03-15 12:40:01 -0300

    compilar TS novamente

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@
 // Imports modules
 import app from "./server";
 
-const PORT: number = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`server is running on http://localhost:${PORT}`);
 });
