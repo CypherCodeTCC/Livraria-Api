56b3476e1a92e7af4927ac9cde0a8a1a979840c6
3DUTEX <t3xdev@gmail.com>
2024-03-18 09:39:46 -0300

    adicionar vercel.json

--- dist/database/supabase.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const supabase_js_1 = require("@supabase/supabase-js");
-const supabase = (0, supabase_js_1.createClient)(process.env.supabaseURL, process.env.supabaseKEY);
-exports.default = supabase;
-//# sourceMappingURL=supabase.js.map
